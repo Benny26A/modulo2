81a76ccaeeafa36129d607de445306f3964d444e
Angella Costa <angella.costa@pditabira.com.br>
2026-08-10 16:10:55 -0400

    Add files via upload

---
+++ aula2_questao1.py
@@ -0,0 +1,7 @@
+num1=5
+num2=2
+print(type(num1))
+print(type(num2))
+div=((num1/num2))
+print(div)
+print("é", type(div))
---
+++ aula2_questao2.py
@@ -0,0 +1,8 @@
+result= "o resultado é"
+value= 10
+value_2=3.5
+sum=(value+value_2)
+print(value)
+print(value_2)
+print((result), (sum))
+print(type(result), type(value), type(value_2), type(sum))
